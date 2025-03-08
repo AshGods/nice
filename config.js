05f967035ad436ee2be414fabf7e47b25b4b81dc
AshGods <156516511+AshGods@users.noreply.github.com>
2025-03-08 16:57:12 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,46 @@
+window.settings = {
+  // 站点名称
+  title: '微斯人，吾谁与归',
+  // 站点描述
+  description: '让我们一路同行',
+  // 登录页背景图
+  background_url: 'https://www.loliapi.com/acg/',
+  // Crisp 客服系统网站 ID
+  crisp_id: '7056abf8-bbff-44dd-badd-9c475b2730e3',
+  // 客户端配置，只能修改 icon 和 url内容
+  download: {
+    iOS: {
+      icon: "material-symbols:ios-rounded",
+      url: "https://apps.apple.com/ca/app/shadowrocket/id932747118",
+    },
+    Windows: {
+      icon: "material-symbols:window",
+      url: "https://d.ash.yt/d/ashnet/client/Ash.exe?sign=EXY88GmWXf7EW3fcHo3CX48ofC-gvKDgkG40EWVfxfs=:0",
+    },
+    Android: {
+      icon: "material-symbols:android",
+      url: "https://d.ash.yt/d/ashnet/client/Ash.apk?sign=7Sj9CBQIC9MAq_Krud7bSlVlrprw1ntiUhyeP29O9AQ=:0",
+    },
+    "Mac OS": {
+      icon: "material-symbols:desktop-mac-outline-rounded",
+      url: "https://d.ash.yt/d/ashnet/client/Ash.dmg?sign=UJdHEVob-9j76rJH7tHHApqmUNbqIJxCQvNDdRfxnJk=:0",
+    }
+  },
+  // 额外配置
+  extend_options: {
+    // 是否为新版 V2Board 
+    isNewV2board: true,
+    // 是否禁用用户工单
+    disableTicket: false,
+  },
+  // 除非你明确知道浏览器名称，否则请不要修改
+  disabled_browser: [
+    'WeChat', 'weibo'
+  ],
+  notice: "测试通知公告",
+  knowledge: "https://air.ash.yt/",
+  telegram: {
+    group: "https://t.me/Ash_Net",
+    channel: "https://t.me/Ash_NOC",
+  }
+}
